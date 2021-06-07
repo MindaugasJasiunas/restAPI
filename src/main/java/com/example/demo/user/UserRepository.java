8d0d629ac63a25938c70fe15e25acf6c24b8700c
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-06-07 18:47:16 +0300

    User Repository updated. #7

--- src/main/java/com/example/demo/user/UserRepository.java
+++ src/main/java/com/example/demo/user/UserRepository.java
@@ -1,10 +1,20 @@
 package com.example.demo.user;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
     Optional<UserEntity> findUserEntityByPublicId(UUID publicId);
+
+    @Transactional //(service method needs @Transactional too)
+    @Modifying //modifying query
+    Optional<UserEntity> deleteUserEntityByPublicId(UUID publicId);
+
+    boolean existsByEmail(String email);
+
+    Optional<UserEntity> findUserEntityByEmail(String email);
 }
