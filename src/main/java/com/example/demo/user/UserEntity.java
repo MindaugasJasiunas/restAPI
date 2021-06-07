86b2ff107819c095a7cc763ab612f2b4abce3d8e
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-06-07 13:14:16 +0300

    Created User entity. Closes #6

--- src/main/java/com/example/demo/user/UserEntity.java
+++ src/main/java/com/example/demo/user/UserEntity.java
@@ -1,4 +1,25 @@
 package com.example.demo.user;
 
-public class UserEntity {
+import com.example.demo.entity.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+
+@Entity(name = "User")
+@Table(name = "user")
+public class UserEntity extends BaseEntity {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String encryptedPassword;
+    private String emailVerificationToken;
+    private boolean emailVerificationStatus=false;
+
 }
