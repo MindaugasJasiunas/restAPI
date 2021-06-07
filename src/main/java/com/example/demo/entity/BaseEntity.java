01a73b824a421d41bde1cc6b539b76343dc0b569
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-06-07 13:13:13 +0300

    Created base class for entities. Closes #9

---
+++ src/main/java/com/example/demo/entity/BaseEntity.java
@@ -0,0 +1,43 @@
+package com.example.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Setter
+@Getter
+
+@MappedSuperclass
+public class BaseEntity implements Serializable{
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @SequenceGenerator( name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+    private Long id;
+
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID publicId= UUID.randomUUID();
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedAt;
+
+    public boolean isNew(){
+        return this.id==null;
+    }
+}
