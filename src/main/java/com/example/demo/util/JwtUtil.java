efa4411fcd51edd5a8f2aea0f13deb3844fa20e9
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-06-09 15:01:16 +0300

    Changed to component for usage in spring framework #32

--- src/main/java/com/example/demo/util/JwtUtil.java
+++ src/main/java/com/example/demo/util/JwtUtil.java
@@ -4,14 +4,16 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+@Component
 public class JwtUtil {
-    private String SECRET_kEY= "secret_secret_secret_secret_secret_secret_secret_secret_secret";
+    private String SECRET_KEY= "secret_secret_secret_secret_secret_secret_secret_secret_secret";
 
     public String extractUsername(String token){
         return extractClaim(token, Claims::getSubject);
@@ -27,7 +29,7 @@ public class JwtUtil {
     }
 
     private Claims extractAllClaims(String token){
-        return Jwts.parser().setSigningKey(SECRET_kEY).parseClaimsJws(token).getBody();
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
     }
 
     private Boolean isTokenExpired(String token){
@@ -49,7 +51,7 @@ public class JwtUtil {
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*10)) // 10 hour
                 //sign
-                .signWith(SignatureAlgorithm.HS512, SECRET_kEY)
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
                 .compact();
     }
 
