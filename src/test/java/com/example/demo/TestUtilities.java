a625ef319f6c52127eb07d72b9e0199d2ab46cbd
Mindaugas Jasiunas <mindaugas.jasiunas.it@gmail.com>
2021-06-10 17:52:50 +0300

    Utility class to use in User Service test. #40

---
+++ src/test/java/com/example/demo/TestUtilities.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+import com.example.demo.user.UserEntity;
+import org.springframework.security.core.userdetails.User;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class TestUtilities {
+
+    public static UserEntity generateUserEntity(){
+        UserEntity userEntity1= new UserEntity();
+        userEntity1.setId(1L);
+        userEntity1.setPublicId(UUID.randomUUID());
+        userEntity1.setEmail("john.doe@example.com");
+        userEntity1.setFirstName("John");
+        userEntity1.setLastName("Doe");
+        userEntity1.setEncryptedPassword("encryptedPassword");
+        userEntity1.setEmailVerificationToken("ABCDEFG");
+        userEntity1.setEmailVerificationStatus(true);
+        userEntity1.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        userEntity1.setLastModifiedAt(new Timestamp(System.currentTimeMillis()+3600000));
+        return userEntity1;
+    }
+
+    public static List<UserEntity> generateUserEntitiesList(){
+        List<UserEntity> userEntities= new ArrayList<>();
+
+        UserEntity userEntity1= new UserEntity();
+        userEntity1.setId(1L);
+        userEntity1.setPublicId(UUID.randomUUID());
+        userEntity1.setEmail("john.doe@example.com");
+        userEntity1.setFirstName("John");
+        userEntity1.setLastName("Doe");
+        userEntity1.setEncryptedPassword("encryptedPassword");
+        userEntity1.setEmailVerificationToken("ABCDEFG");
+        userEntity1.setEmailVerificationStatus(true);
+        userEntity1.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        userEntity1.setLastModifiedAt(new Timestamp(System.currentTimeMillis()+3600000));
+        userEntities.add(userEntity1);
+
+        UserEntity userEntity2= new UserEntity();
+        userEntity2.setId(2L);
+        userEntity2.setPublicId(UUID.randomUUID());
+        userEntity2.setEmail("jane.does@example.com");
+        userEntity2.setFirstName("Jane");
+        userEntity2.setLastName("Does");
+        userEntity2.setEncryptedPassword("password");
+        userEntity2.setEmailVerificationToken("GFEDCBA");
+        userEntity2.setEmailVerificationStatus(true);
+        userEntity2.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        userEntity2.setLastModifiedAt(new Timestamp(System.currentTimeMillis()+3600000));
+        userEntities.add(userEntity2);
+
+        return userEntities;
+    }
+
+}
